03beb19077706111f9697a58da72f95a19f1fe87
andersonbarbosaeae <115184454+andersonbarbosaeae@users.noreply.github.com>
2023-03-14 21:22:11 +0100

    Update 🚀-Winning-Horse.py

--- 🚀-Winning-Horse.py
+++ 🚀-Winning-Horse.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(page_title="Winning Horse", page_icon=":bar_chart:", layout="wide")
 
 # Title
-st.title("🚀 Winning Horses")
+st.title("🚀 Winning Horse")
 
 # Content
 st.subheader("🔥 Motivación")
